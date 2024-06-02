1ad3a10732e5e373a12422bc1665cf977fea9b74
Axel Carlsson <axelthescarecrow@gmail.com>
2024-06-02 15:34:06 +0200

    Header broken out into own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
-"use client";
+import Header from "@/components/Header";
 import Posts from "@/components/Posts";
-import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 const Page: FC = ({ }) => {
-    const router = useRouter();
-
-    const logout = async () => {
-        router.push("/login");
-    }
 
     return (
         <>
-            <header>
-                <div className="grid grid-cols-[1fr_auto_1fr] py-2 px-4 shadow">
-                    <div></div>
-                    <div className="text-center p-4 text-2xl group bg-gradient-to-r from-black to-indigo-500 bg-clip-text text-transparent">
-                        GOPHER MEDIA
-                    </div>
-                    <button className="ml-auto hover:text-indigo-500" onClick={() => logout()}>
-                        Logout
-                    </button>
-                </div>
-            </header>
+        <Header></Header>
             <div className="pt-8">
                 <Posts />
             </div>
---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+    const router = useRouter();
+
+    const logout = async () => {
+        router.push("/login");
+    }
+
+    return (
+        <header>
+            <div className="grid grid-cols-[1fr_auto_1fr] py-2 px-4 shadow">
+                <div></div>
+                <div className="text-center p-4 text-2xl group bg-gradient-to-r from-black to-indigo-500 bg-clip-text text-transparent">
+                    GOPHER MEDIA
+                </div>
+                <button className="ml-auto hover:text-indigo-500" onClick={() => logout()}>
+                    Logout
+                </button>
+            </div>
+        </header>
+    )
+}
